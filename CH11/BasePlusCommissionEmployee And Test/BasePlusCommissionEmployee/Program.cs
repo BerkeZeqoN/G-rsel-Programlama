dd977e7ad36078bd9558985d51d917b938ef35b7
BerkeZeqoN <berke.codehub@gmail.com>
2025-12-03 15:05:34 +0300

    CH11

---
+++ CH11/BasePlusCommissionEmployee And Test/BasePlusCommissionEmployee/Program.cs
@@ -0,0 +1,112 @@
+﻿// Fig. 11.6: BasePlusCommissionEmployee.cs
+// BasePlusCommissionEmployee class represents an employee that receives
+// a base salary in addition to a commission.
+
+using System;
+
+public class BasePlusCommissionEmployee
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+    public string SocialSecurityNumber { get; }
+
+    private decimal grossSales;     // gross weekly sales
+    private decimal commissionRate; // commission percentage
+    private decimal baseSalary;     // base salary per week
+
+    // six-parameter constructor
+    public BasePlusCommissionEmployee(string firstName, string lastName,
+        string socialSecurityNumber, decimal grossSales,
+        decimal commissionRate, decimal baseSalary)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        SocialSecurityNumber = socialSecurityNumber;
+
+        GrossSales = grossSales;           // validates grossSales
+        CommissionRate = commissionRate;   // validates commissionRate
+        BaseSalary = baseSalary;           // validates baseSalary
+    }
+
+    // property for gross sales
+    public decimal GrossSales
+    {
+        get => grossSales;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value),
+                    value, $"{nameof(GrossSales)} must be >= 0");
+            }
+            grossSales = value;
+        }
+    }
+
+    // property for commission rate
+    public decimal CommissionRate
+    {
+        get => commissionRate;
+        set
+        {
+            if (value <= 0 || value >= 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value),
+                    value, $"{nameof(CommissionRate)} must be > 0 and < 1");
+            }
+            commissionRate = value;
+        }
+    }
+
+    // property for base salary
+    public decimal BaseSalary
+    {
+        get => baseSalary;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value),
+                    value, $"{nameof(BaseSalary)} must be >= 0");
+            }
+            baseSalary = value;
+        }
+    }
+
+    // calculate earnings
+    public decimal Earnings() =>
+        BaseSalary + (CommissionRate * GrossSales);
+
+    // string representation
+    public override string ToString() =>
+        $"base-salaried commission employee: {FirstName} {LastName}\n" +
+        $"social security number: {SocialSecurityNumber}\n" +
+        $"gross sales: {GrossSales:C}\n" +
+        $"commission rate: {CommissionRate:F2}\n" +
+        $"base salary: {BaseSalary:C}";
+        
+}
+class BasePlusCommissionEmployeeTest
+{
+    static void Main()
+    {
+        var employee = new BasePlusCommissionEmployee("Bob", "Lewis", "44-444-4444", 5000.00M, .04M, 300.00M);
+
+        Console.WriteLine(
+            "Employee informations are obtained by properties and methods: \n ");
+        Console.WriteLine($" First Name İs {employee.FirstName}");
+        Console.WriteLine($"Last Name İs {employee.LastName}");
+        Console.WriteLine($"Social Security Number is {employee.SocialSecurityNumber}");
+        Console.WriteLine($"GrossSales are {employee.GrossSales}");
+        Console.WriteLine($" Commision rate is {employee.CommissionRate}");
+        Console.WriteLine($"Employee earning is {employee.Earnings}");
+        Console.WriteLine($"Base Salary is {employee.BaseSalary}");
+        employee.BaseSalary = 1000.00M;
+
+        Console.WriteLine(
+            "\nUpdated empoloyee information obtained by ToString \n ");
+        Console.WriteLine(employee);
+        Console.WriteLine($"Earning is {employee.Earnings()}");
+    }
+
+}
---
+++ CH11/CommissionEmployee And CommissionEmployeeTest/Unit11/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+public class CommissionEmployee : object
+{
+    private decimal grossSales;        // gross weekly sales
+    private decimal commissionRate;    // commission percentage
+
+    public string FirstName { get; }
+    public string LastName { get; }
+    public string SocialSecurityNumber { get; }
+
+    // five-parameter constructor
+    public CommissionEmployee(string firstName, string lastName,
+        string socialSecurityNumber, decimal grossSales,
+        decimal commissionRate)
+    {
+        // implicit call to object constructor occurs here 
+        FirstName = firstName;
+        LastName = lastName;
+        SocialSecurityNumber = socialSecurityNumber;
+
+        GrossSales = grossSales;             // validates gross sales
+        CommissionRate = commissionRate;     // validates commission rate
+    }
+
+    // property that gets and sets commission employee's gross sales
+    public decimal GrossSales
+    {
+        get
+        {
+            return grossSales;
+        }
+        set
+        {
+            if (value < 0) // validation
+            {
+                throw new ArgumentOutOfRangeException(nameof(value),
+                    value, $"{nameof(GrossSales)} must be >= 0");
+            }
+
+            grossSales = value;
+        }
+    }
+
+    // property that gets and sets commission employee's commission rate
+    public decimal CommissionRate
+    {
+        get
+        {
+            return commissionRate;
+        }
+        set
+        {
+            if (value <= 0 || value >= 1) // validation
+            {
+                throw new ArgumentOutOfRangeException(nameof(value),
+                    value, $"{nameof(CommissionRate)} must be > 0 and < 1");
+            }
+
+            commissionRate = value;
+        }
+    }
+
+    // calculate commission employee's pay
+    public decimal Earnings() => grossSales * commissionRate;
+
+    // return string representation of CommissionEmployee object
+    public override string ToString() =>
+        $"commission employee: {FirstName} {LastName}\n" +
+        $"social security number: {SocialSecurityNumber}\n" +
+        $"gross sales: {grossSales:C}\n" +
+        $"commission rate: {commissionRate:F2}";
+
+       class CommisionEmployeeTest
+    {
+        static void Main()
+        {
+            var employee = new CommissionEmployee("Sue", "jones", "222-22-2222", 100000.00M, .06M);
+
+            Console.WriteLine("Employee information obtained by properties  and methods: \n ");
+            Console.WriteLine(($"First name is {employee.FirstName }"));
+            Console.WriteLine($"Last name is {employee.LastName}");
+            Console.WriteLine($"Social Secuity Number is {employee.SocialSecurityNumber}");
+            Console.WriteLine($"Gross Sales are {employee.GrossSales}");
+            Console.WriteLine($"Commision rate is {employee.commissionRate}");
+            Console.WriteLine($"Your Earning is {employee.Earnings}");
+
+            employee.GrossSales = 5000.00M;
+            employee.commissionRate = .1M;
+
+            Console.WriteLine(
+                "\nUpdated employee information obtained by ToString: \n");
+            Console.WriteLine(employee);
+            Console.WriteLine($"earnings: {employee.Earnings}");
+
+        }
+        
+    }
+}
